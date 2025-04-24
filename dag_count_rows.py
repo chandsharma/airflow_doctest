1a13f1ef4a897c164c709c004d487930f6177fb7
Vishal Singh <68636367+vishallsinghh@users.noreply.github.com>
2025-04-24 10:51:01 +0530

    Update dag_count_rows.py

--- dag_count_rows.py
+++ dag_count_rows.py
@@ -4,7 +4,6 @@ from airflow.sensors.base import BaseSensorOperator
 from kubernetes import client, config
 from datetime import datetime, timedelta
 
-# Default arguments
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
@@ -14,47 +13,6 @@ default_args = {
     "retry_delay": timedelta(minutes=5),
 }
 
-# Spark Application specification embedded directly into DAG
-spark_spec = {
-    "apiVersion": "sparkoperator.k8s.io/v1beta2",
-    "kind": "SparkApplication",
-    "metadata": {
-        "name": "orc-count-sparkapp",
-        "namespace": "default"
-    },
-    "spec": {
-        "type": "Python",
-        "mode": "cluster",
-        "image": "apache/spark:3.5.1-python3",
-        "imagePullPolicy": "IfNotPresent",
-        "mainApplicationFile": "https://vishalsparklogs.blob.core.windows.net/orc-data-container/python_script/orc_count.py",
-        "arguments": ["wasbs://orc-data-container@vishalsparklogs.blob.core.windows.net/dummy_data/parquet/"],
-        "sparkVersion": "3.5.1",
-        "restartPolicy": {
-            "type": "OnFailure",
-            "onFailureRetries": 3,
-            "onFailureRetryInterval": 30
-        },
-        "sparkConf": {
-            "spark.eventLog.enabled": "true",
-            "spark.eventLog.dir": "wasbs://spark-logs@vishalsparklogs.blob.core.windows.net/spark-logs",
-            "spark.hadoop.fs.azure.account.key.vishalsparklogs.blob.core.windows.net": "XZfQviaXeNqQVHjTD6Cwg1VbiUK8YhDWqOSTDskYv5oFd4YzfajqGUHZBE3/2My1mw9hPXfeceYn+AStsFBh7A=="
-        },
-        "driver": {
-            "cores": 1,
-            "memory": "2g",
-            "serviceAccount": "spark",
-            "labels": {}
-        },
-        "executor": {
-            "cores": 2,
-            "instances": 3,
-            "memory": "4g"
-        }
-    }
-}
-
-# Define DAG
 dag = DAG(
     "dag_count_orc_rows_spark_operator",
     default_args=default_args,
@@ -64,13 +22,10 @@ dag = DAG(
     catchup=False,
 )
 
-# Submit SparkApplication using SparkKubernetesOperator
 submit_spark_job = SparkKubernetesOperator(
     task_id="submit_spark_orc_count",
     namespace="default",
-    application_file=spark_spec,
+    application_file="https://vishalsparklogs.blob.core.windows.net/orc-data-container/yaml/spark_orc_count.yaml",
     get_logs=True,
     dag=dag
 )
-
-submit_spark_job
